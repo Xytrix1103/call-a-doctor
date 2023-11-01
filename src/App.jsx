48276837c32700af07cd00e8b8611f3572c5cdef
Ho Shu Yi <syho031001@gmail.com>
2023-11-01 12:41:48 +0800

    export navbar to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import {Outlet} from "react-router-dom";
 import React from "react";
+import {Flex} from "@chakra-ui/react";
+import PatientNavbar from "./components/navbars/PatientNavbar.jsx";
 
 function App() {
 	return (
-		<Outlet/>
+		<Flex display="grid" gridTemplateRows="auto 1fr" w="100%" h="100%" bg="#f4f4f4" overflow="hidden">
+			<PatientNavbar/>
+			<Flex w="100%" h="100%" bg="#f4f4f4" overflow="scroll" p={5}>
+				<Outlet/>
+			</Flex>
+		</Flex>
 	)
 }
 
