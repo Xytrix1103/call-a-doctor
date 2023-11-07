bd75e7a9706bb9d73a7ff76edfc9f250f4dcb033
ChngWeiKean <129861018+ChngWeiKean@users.noreply.github.com>
2023-11-07 10:44:13 +0800

    map update, add info window,

--- src/pages/auth/ClinicRegistry.jsx
+++ src/pages/auth/ClinicRegistry.jsx
@@ -23,81 +23,134 @@ import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 import { useForm } from "react-hook-form";
 import { register_clinic } from "../../../api/clinic_registry.js";
 import { BiSearchAlt2 } from "react-icons/bi";
-import { LoadScript, Marker, GoogleMap, Autocomplete } from '@react-google-maps/api';
+import { LoadScript, Marker, GoogleMap, Autocomplete, InfoWindow } from '@react-google-maps/api';
+import { Link } from 'react-router-dom';
 
-function MapWithSearch () {
+function MapWithSearch() {
 	const mapStyle = {
-		height: '400px',
-		width: '100%',
+	  height: '400px',
+	  width: '100%',
 	};
+	const [mapRef, setMapRef] = useState(null);
 	const [selected, setSelected] = useState(null);
+	const [openInfo, setOpenInfo] = useState(false);
 	const [center, setCenter] = useState({
-		lat: 5.4164,
-		lng: 100.3327
+	  lat: 5.4164,
+	  lng: 100.3327,
 	});
 	const inputRef = useRef();
-
+  
 	const handlePlaceSelect = () => {
 		if (inputRef.current && inputRef.current.getPlace) {
-		  const place = inputRef.current.getPlace();
-		  const { geometry, formatted_address } = place;
-		  const { location } = geometry;
-		  console.log(place);
-		  console.log(location.lat(), location.lng(), formatted_address);
-		  setCenter({ lat: location.lat(), lng: location.lng() });
-		  setSelected({ lat: location.lat(), lng: location.lng() });
+			const place = inputRef.current.getPlace();
+			const placeId = place.place_id;
+			const { geometry, formatted_address, name } = place;
+			const { location } = geometry;
+			mapRef.panTo({ lat: location.lat(), lng: location.lng() });
+			setSelected({
+				lat: location.lat(),
+				lng: location.lng(),
+				name: name,
+				address: formatted_address,
+				placeId: placeId,
+			});
+			setOpenInfo(true);
 		}
 	};
 
+	const handleViewOnMaps = () => {
+		if (selected) {
+			const { name } = selected;
+			const url = `https://www.google.com/maps/search/?api=1&query=${name}`;
+			window.open(url, '_blank');
+		}
+	};
+  
 	return (
 		<LoadScript
 			googleMapsApiKey="AIzaSyCxkZ_qonH-WY9cbiHZsUgp9lE3PdkWH_A"
 			libraries={["places"]}
 		>
-			<Box>
-				<Box
-					mb={2}
-					mt={2}
-					w="full"
+				
+			<Box
+				mb={2}
+				mt={2}
+				w="full"
+			>
+				<Autocomplete
+					onLoad={(autocomplete) => {
+						inputRef.current = autocomplete;
+						autocomplete.setFields(["geometry", "formatted_address", "place_id", "name"]);
+					}}
+					onPlaceChanged={handlePlaceSelect}
 				>
-					<Autocomplete
-						onLoad={(autocomplete) => {
-							inputRef.current = autocomplete;
-							autocomplete.setFields(["geometry", "formatted_address"]);
-						}}
-						onPlaceChanged={handlePlaceSelect}
-					>
-						<InputGroup size="md">
-							<InputLeftElement
-								pointerEvents="none"
-								children={<BiSearchAlt2 color="gray.500" />}
-							/>
-							<Input 
-								type='text' 
-								placeholder="Search for clinic location.."
-								ref={inputRef}
-								focusBorderColor='blue.500'
-							/>
-						</InputGroup>         
-					</Autocomplete>
-				</Box>
-				<GoogleMap
-					center={center}
-					zoom={20}
-					mapContainerStyle={mapStyle}
-					options={{ 
-						streetViewControl: false,
-						mapTypeControl: false,
-					 }}
-				>
-					{selected && <Marker position={selected} />}
-				</GoogleMap>
+					<InputGroup size="md">
+						<InputLeftElement
+							pointerEvents="none"
+							children={<BiSearchAlt2 color="gray.500" />}
+						/>
+						<Input 
+							type='text' 
+							placeholder="Search for clinic location.."
+							ref={inputRef}
+							focusBorderColor='blue.500'
+						/>
+					</InputGroup>         
+				</Autocomplete>
 			</Box>
+			<GoogleMap
+				onLoad={(map) => {
+					setMapRef(map);
+				}}
+				center={center}
+				zoom={15} // Adjust the zoom level as needed
+				mapContainerStyle={mapStyle}
+			>
+				{selected && (
+					<Marker
+						position={selected}
+						onClick={() => {
+							// Handle marker click
+							setOpenInfo(!openInfo);
+						}}
+					/>
+				)}
+				{openInfo && selected && (
+					<InfoWindow
+						position={{ lat: selected.lat + 0.0015, lng: selected.lng }}
+						onCloseClick={() => {
+							setOpenInfo(false);
+						}}
+					>
+						<Box p={1} maxW="200px">
+							<Text fontSize="sm" fontWeight="medium">
+								{selected.name}
+							</Text>
+							<Text fontSize="xs" fontWeight="medium" color="gray.500" mt={1} mb={2}>
+								{selected.address}
+							</Text>
+							<Link onClick={handleViewOnMaps} size="sm" fontSize="xs">
+								View on Google Maps
+							</Link>
+						</Box>
+					</InfoWindow>
+				)}
+			</GoogleMap>
 		</LoadScript>
 	);
 }
 
 function ClinicRegistry() {
+	const steps = [
+		{ title: 'First', description: 'Clinic Address' },
+		{ title: 'Second', description: 'Clinic Registry' },
+		{ title: 'Third', description: 'Admin Registry' },
+	];
+	// const { activeStep, setActiveStep } = useSteps({
+	// 	index: 1,
+	// 	count: steps.length,
+	// })
+	
 	const {
 		handleSubmit,
 		register,
